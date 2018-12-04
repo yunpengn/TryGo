05b039a89131deb819b0247bc8d2b4a474ff1e8c
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-12-05 00:31:57 +0800

    Add newline char

--- src/0_hello_world.go
+++ src/0_hello_world.go
@@ -8,4 +8,6 @@ This is a block of comment(s), multiple lines.
 func main() {
 	// This is a comment
 	fmt.Println("Hello, world! Let's go using Go!")
+	// This is another comment
+	fmt.Print("You can also use a manual newline character.\n")
 }
