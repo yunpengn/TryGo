27f6e417d59e7bfb0c890764bedbe79bb773960a
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-12-09 11:24:33 +0800

    Add const

--- src/1_variable.go
+++ src/1_variable.go
@@ -2,10 +2,32 @@ package main
 
 import "fmt"
 
+var (
+	some int
+	thing string
+)
+
+// go-lang has type inference, similar to Swift. It also assigns a default value
+// to all variables declared without an initial value.
 func main() {
 	var a int = 10
 	var b = 9
 	c := 8
 
+	// Swap the two values.
+	a, b = b,a
+
+	// Notice: if we declare a variable without its intial value, we have to assign
+	// a type to it (and thus it will be automatically initialized to the default
+	// value of that type).
+	var d bool
+
 	fmt.Println("The sum is", (a + b + c))
+	fmt.Println("The default value of d is", d)
+
+	// We can declare multiple variables in one line.
+	var e, f = "45", 67
+
+	fmt.Println("The values are", e, "and", f)
+	fmt.Println("The address of e is", &e)
 }
---
+++ src/2_constant.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const LENGTH int = 10
+	// A constant has to be declared with an initial value.
+	const DECISION = false
+
+	fmt.Println("Since the length is", LENGTH, ". The decision is", DECISION)
+}
