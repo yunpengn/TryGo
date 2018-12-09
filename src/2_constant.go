00be6f1ab5c4bd0526ca3bcec8fcefd06aa6c5b8
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-12-09 11:30:18 +0800

    Add iota

--- src/2_constant.go
+++ src/2_constant.go
@@ -1,11 +1,31 @@
 package main
 
 import "fmt"
+import "unsafe"
+
+const (
+	a = "abc"
+	b = len(a)
+	c = unsafe.Sizeof(a)
+)
+
+const (
+	d = iota
+	e
+	f = "haha"
+	g
+	h = 100
+	i
+	j = iota
+	k
+)
 
 func main() {
 	const LENGTH int = 10
-	// A constant has to be declared with an initial value.
+	// A constant has to be declared with an initial value (since it is constant whose value cannot change anymore).
 	const DECISION = false
 
 	fmt.Println("Since the length is", LENGTH, ". The decision is", DECISION)
+	fmt.Println(a, b, c)
+	fmt.Println(d, e, f, g, h, i, j, k)
 }
